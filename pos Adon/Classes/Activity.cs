30cff5cbf9f7cd4cfef49cc450d000261c190c19
DillibabuK <dillibabu@sociusus.com>
2024-10-15 14:49:24 +0530

    Starting

---
+++ pos Adon/Classes/Activity.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using General.Classes;
+using pos_Adon;
+using SAPbouiCOM;
+using static General.Classes.@enum;
+
+namespace pos_Adon.Classes
+{
+    class Activity:Connection
+    {
+        #region Variables  
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        SAPbouiCOM.Form oForm;
+        SAPbouiCOM.Matrix oMatrix;
+        SAPbouiCOM.EditText oEdit;
+        SAPbouiCOM.ComboBox oCombo;
+        SAPbouiCOM.Item oItem;
+        SAPbouiCOM.Item oNewItem;
+        SAPbouiCOM.Button obutton;
+        public const string objType = "OCLG";
+        SAPbouiCOM.DBDataSource oDbDataSource = null;
+        const string formMenuUID = "OCLG";
+        //const string formMenuUID1 = "SOF";
+        public const string formTypeEx = "OCLG";
+        clsCommon objclsComman = new clsCommon();
+        StringBuilder sbQuery = new StringBuilder();
+
+        public const string headerTable = "OCLG";
+      
+        #endregion
+
+        public void MenuEvent(ref SAPbouiCOM.MenuEvent pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            try
+            {
+                #region BeforeAction == true
+
+                if (pVal.BeforeAction == true)
+                {
+                    try
+                    {
+                        //if (pVal.MenuUID == formMenuUID || pVal.MenuUID == Convert.ToString((int)SAPMenuEnum.AddRecord))
+                        //{
+                        //    oForm = oApplication.Forms.ActiveForm;
+                        //    if (oForm.Mode == SAPbouiCOM.BoFormMode.fm_ADD_MODE)
+                        //    {
+                        //        //Record is directly added without validation
+                        //        BubbleEvent = false;
+                        //    }
+                        //}
+                  
+                    }
+                    catch (Exception ex)
+                    {
+                        SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " Before action = true : " + ex.Message);
+                        oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+                    }
+                }
+                #endregion
+
+                #region BeforeAction == false
+
+                if (pVal.BeforeAction == false)
+                {
+                    try
+                    {
+                        if (pVal.MenuUID == formMenuUID || pVal.MenuUID == Convert.ToString((int)SAPMenuEnum.AddRecord))
+                        {
+                            LoadForm(pVal.MenuUID);
+                        }
+                       
+
+                    }
+                    catch (Exception ex)
+                    {
+                        SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " Before action = false : " + ex.Message);
+                        oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+                    }
+                }
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message);
+                oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+            }
+
+        }
+
+
+        public void ItemEvent(ref SAPbouiCOM.ItemEvent pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            try
+            {
+                #region Before_Action == true
+                if (pVal.BeforeAction == true)
+                {
+                    try
+                    {
+                        oForm = oApplication.Forms.Item(pVal.FormUID);
+                        if (pVal.EventType == SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
+                        {
+                           
+                            if (pVal.ItemUID == "btnExcel")
+                            {
+                                ExcelApp(@"C:\Users\socius7\source\repos\pos Adon\Files\PosForm.xlsx");
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message);
+                        oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+                    }
+                }
+                #endregion
+
+                #region Before_Action == false
+                if (pVal.BeforeAction == false)
+                {
+                    try
+                    {
+                        if (pVal.EventType == SAPbouiCOM.BoEventTypes.et_FORM_LOAD)
+                        {
+                            oForm = oApplication.Forms.Item(pVal.FormUID);
+                        
+                            oNewItem = oForm.Items.Add("btnExcel", SAPbouiCOM.BoFormItemTypes.it_BUTTON);
+                            oItem = oForm.Items.Item("2"); 
+                            obutton = oNewItem.Specific;
+                            obutton.Caption = "Open Excel";
+                            oNewItem.Top = oItem.Top;  
+                            oNewItem.Left = oItem.Left + oItem.Width + 5;  
+                            oNewItem.Height = oItem.Height;
+                            oNewItem.Width = oItem.Width;
+                            oNewItem.FromPane = 0;
+                            oNewItem.ToPane = 0;
+
+                        }
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " Before action = false : " + ex.Message);
+                        oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+                    }
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " Before action = false : " + ex.Message);
+                oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+            }
+        }
+
+
+
+        #region Methods
+        public void LoadForm(string MenuID)
+        {
+            clsVariables.boolCFLSelected = false;
+
+            if (MenuID == formMenuUID)
+            {
+                string formUID = "";
+                objclsComman.LoadXML(MenuID, "", string.Empty, SAPbouiCOM.BoFormMode.fm_ADD_MODE);
+                oForm = oApplication.Forms.ActiveForm;
+                oForm.DataSources.UserDataSources.Add("Close", SAPbouiCOM.BoDataType.dt_SHORT_TEXT, 10);
+                oForm.DataSources.UserDataSources.Item("Close").Value = "N";
+
+            }
+
+
+            oForm = oApplication.Forms.ActiveForm;
+           
+         
+        }
+
+        public void ExcelApp()
+        {
+
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+
+
+            try
+            {
+                excelApp.Visible = true;
+                Microsoft.Office.Interop.Excel.Workbook workbook = excelApp.Workbooks.Add();
+
+                Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets[1]; // Access the first sheet
+
+                worksheet.Name = "Sheet1";
+
+                Thread.Sleep(1000);  // Adjust the delay as necessary
+
+                // Find the Excel process by its name
+                Process[] processes = Process.GetProcessesByName("EXCEL");
+                if (processes.Length > 0)
+                {
+                    // Get the main window handle of Excel
+                    IntPtr excelHandle = processes[0].MainWindowHandle;
+
+                    if (excelHandle != IntPtr.Zero)
+                    {
+                        // Bring Excel to the front
+                        bool success = SetForegroundWindow(excelHandle);
+                        if (!success)
+                        {
+                            Console.WriteLine("Failed to bring Excel to the front.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Excel window handle is not available.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Excel process not found.");
+                }
+
+
+                // Now, you can work with the sheet. For example, write something to a cell.
+                // worksheet.Cells[1, 1].Value = "Hello, Excel!";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error opening Excel file: " + ex.Message);
+            }
+            finally
+            {
+                // Optionally, you can close the workbook after use
+                // workbook.Close(false); // Set to true to save changes
+                // excelApp.Quit();
+            }
+        }
+
+        public void ExcelApp(string FilePath)
+        {
+
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+
+
+            try
+            {
+                excelApp.Visible = true;
+                Microsoft.Office.Interop.Excel.Workbook workbook = excelApp.Workbooks.Open(FilePath);
+
+              
+                Thread.Sleep(1000);  // Adjust the delay as necessary
+
+                // Find the Excel process by its name
+                Process[] processes = Process.GetProcessesByName("EXCEL");
+                if (processes.Length > 0)
+                {
+                    // Get the main window handle of Excel
+                    IntPtr excelHandle = processes[0].MainWindowHandle;
+
+                    if (excelHandle != IntPtr.Zero)
+                    {
+                        // Bring Excel to the front
+                        bool success = SetForegroundWindow(excelHandle);
+                        if (!success)
+                        {
+                            Console.WriteLine("Failed to bring Excel to the front.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Excel window handle is not available.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Excel process not found.");
+                }
+
+
+                // Now, you can work with the sheet. For example, write something to a cell.
+                // worksheet.Cells[1, 1].Value = "Hello, Excel!";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error opening Excel file: " + ex.Message);
+            }
+            finally
+            {
+                // Optionally, you can close the workbook after use
+                // workbook.Close(false); // Set to true to save changes
+                // excelApp.Quit();
+            }
+        }
+
+
+
+
+        #endregion
+    }
+}
---
+++ pos Adon/Classes/SAPMain.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using General;
+using General.Classes;
+using pos_Adon;
+using General.Extensions;
+using log4net;
+using log4net.Config;
+using log4net.Repository.Hierarchy;
+using SAPbouiCOM;
+using System.Reflection;
+using pos_Adon.Classes;
+using static General.Classes.@enum;
+
+
+namespace pos_Adon
+{
+    class SAPMain : Connection
+    {
+        public static ILog logger;
+        SAPbouiCOM.Form oForm;
+
+        #region Variables
+        //the below lines will be given then only on the PreparMenus we can access objclsComman sowhen copy&paste 
+        //it will show you the error so copying the below lines from the Reference code
+        clsCommon objclsComman = new clsCommon();
+
+        //BPIMaster objBPIMaster = new BPIMaster();
+        //CCBI objCCBI = new CCBI();
+        //BOUR objBOUR = new BOUR();
+        //DCBA objDCBA = new DCBA();
+        Activity objactivity = new Activity();
+        
+        #endregion
+
+        #region Constructor
+
+        public SAPMain()
+        {
+            InitLogger();
+            ConnectToSAPApplication();
+            PrepareMenus();
+            PrepareEvents();
+        }
+
+        private void PrepareMenus()
+        {
+            logger.DebugFormat("> {0}", nameof(PrepareMenus));
+            string superUser = objclsComman.SelectRecord("SELECT SuperUser FROM OUSR WHERE UserId='" + oCompany.UserSignature + "'");
+           // if (superUser == YesNoEnum.Y.ToString())
+           // {
+              //  objclsComman.AddMenu(BoMenuType.mt_STRING, Convert.ToString((int)SAPMenuEnum.SystemInitialisation), SAPCustomFormUIDEnum.BPIUDO.ToString(), SAPCustomFormUIDEnum.BPIUDO.ToDescription(), "BPIUDO", 0);
+            //}
+            //if (isLiceseExpired == false)
+            //{
+            //    objclsComman.AddMenu(BoMenuType.mt_POPUP, Convert.ToString((int)SAPMenuEnum.Modules), SAPCustomFormUIDEnum.BPIUDO.ToString(), SAPCustomFormUIDEnum.BPIADO.ToDescription(), "BPIUDO", 16);//SAPCustomFormUIDEnum.BPIADO.ToString()
+            //    objclsComman.AddMenu(BoMenuType.mt_STRING, SAPCustomFormUIDEnum.BPIADO.ToString(), SAPCustomFormUIDEnum.BPIM.ToString(), SAPCustomFormUIDEnum.BPIM.ToDescription(), "BPIUDO", 0);
+            //    objclsComman.AddMenu(BoMenuType.mt_STRING, SAPCustomFormUIDEnum.BPIADO.ToString(), SAPCustomFormUIDEnum.CCBI.ToString(), SAPCustomFormUIDEnum.CCBI.ToDescription(), "BPIUDO", 1);
+            //    objclsComman.AddMenu(BoMenuType.mt_STRING, SAPCustomFormUIDEnum.BPIADO.ToString(), SAPCustomFormUIDEnum.BOUR.ToString(), SAPCustomFormUIDEnum.BOUR.ToDescription(), "BPIUDO", 2);
+            //    objclsComman.AddMenu(BoMenuType.mt_STRING, SAPCustomFormUIDEnum.BPIADO.ToString(), SAPCustomFormUIDEnum.DCBA.ToString(), SAPCustomFormUIDEnum.DCBA.ToDescription(), "BPIUDO", 3);
+            //}
+        }
+
+        void RemoveMenusFromSAP()
+        {
+            objclsComman.RemoveMenu(SAPCustomFormUIDEnum.BPIUDO.ToString());
+          // objclsComman.RemoveMenu(SAPCustomFormUIDEnum.BPIADO.ToString());
+           // objclsComman.RemoveMenu(SAPCustomFormUIDEnum.BPIM.ToString());
+       
+        }
+
+        /// <summary>
+        /// Create Event Handler 
+        /// </summary>
+        void PrepareEvents()
+        {
+            logger.DebugFormat("> {0} ", nameof(PrepareEvents));
+           oApplication.ItemEvent += new _IApplicationEvents_ItemEventEventHandler(oApplication_ItemEvent);
+            oApplication.MenuEvent += new _IApplicationEvents_MenuEventEventHandler(oApplication_MenuEvent);
+            oApplication.FormDataEvent += new SAPbouiCOM._IApplicationEvents_FormDataEventEventHandler(oApplication_FormDataEvent);
+            oApplication.AppEvent += new _IApplicationEvents_AppEventEventHandler(oApplication_AppEvent);
+            oApplication.LayoutKeyEvent += new _IApplicationEvents_LayoutKeyEventEventHandler(oApplication_LayoutKeyEvent);
+        }
+
+        #endregion
+
+        #region Events
+
+   
+        void oApplication_ItemEvent(string FormUID, ref SAPbouiCOM.ItemEvent pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            try
+            {
+
+                if (pVal.FormTypeEx == Convert.ToString((int)SAPFormUIDEnum.Activity))
+                {
+                    objactivity.ItemEvent(ref pVal, out BubbleEvent);
+                }
+                // if (pVal.FormTypeEx == SAPCustomFormUIDEnum.CCBI.ToString())
+                //{
+                //    objCCBI.ItemEvent(ref pVal, out BubbleEvent);
+                //}
+                //if (pVal.FormTypeEx == SAPCustomFormUIDEnum.BOUR.ToString())
+                //{
+                //    objBOUR.ItemEvent(ref pVal, out BubbleEvent);
+                //}
+                //if (pVal.FormTypeEx == SAPCustomFormUIDEnum.DCBA.ToString())
+                //{
+                //    objDCBA.ItemEvent(ref pVal, out BubbleEvent);
+                //}
+                //else if (pVal.FormTypeEx == SAPCustomFormUIDEnum.SOF.ToString() || (oForm != null && oForm.TypeEx == SAPCustomFormUIDEnum.SOF.ToString()))
+
+                //{
+                //    objCCBI.ItemEvent(ref pVal, out BubbleEvent);
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message);
+                oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+            }
+        }
+
+        //.....
+        //.....
+
+        void oApplication_MenuEvent(ref SAPbouiCOM.MenuEvent pVal, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            try
+            {
+                try
+                {
+                    oForm = oApplication.Forms.ActiveForm;
+                }
+                catch
+
+
+                {
+                    oForm = null;
+                }
+
+                if (pVal.BeforeAction == true)
+                {
+                    if (pVal.MenuUID == SAPCustomFormUIDEnum.BPIUDO.ToString())
+                    {
+                        objclsComman.ExcelApp();
+                       // oApplication.StatusBar.SetText("Object Tables Created successfully!", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
+                    }
+                }
+
+                //Below is the code for accessing Menu Events
+
+              //  if (pVal.MenuUID == Convert.ToString((int)SAPMenuEnum.Activity) || (oForm != null && oForm.TypeEx == Convert.ToString((int)SAPFormUIDEnum.Activity)))
+             //   {
+                    //BPIMaster.(ref pVal, out BubbleEvent);
+               //     objactivity.MenuEvent(ref pVal, out BubbleEvent);
+             //   }
+                if (oForm.TypeEx == Convert.ToString((int)SAPFormUIDEnum.Activity))
+                {
+                    objactivity.MenuEvent(ref pVal, out BubbleEvent);
+                }
+                // if (pVal.MenuUID == SAPCustomFormUIDEnum.CCBI.ToString() || (oForm != null && oForm.TypeEx == SAPCustomFormUIDEnum.CCBI.ToString()))
+                //{
+                //    //BPIMaster.(ref pVal, out BubbleEvent);
+                //    objCCBI.MenuEvent(ref pVal, out BubbleEvent);
+                //}
+                //if (pVal.MenuUID == SAPCustomFormUIDEnum.BOUR.ToString() || (oForm != null && oForm.TypeEx == SAPCustomFormUIDEnum.BOUR.ToString()))
+                //{
+                //    objBOUR.MenuEvent(ref pVal, out BubbleEvent);
+                //}
+                //if (pVal.MenuUID == SAPCustomFormUIDEnum.DCBA.ToString() || (oForm != null && oForm.TypeEx == SAPCustomFormUIDEnum.DCBA.ToString()))
+                //{
+                //    objDCBA.MenuEvent(ref pVal, out BubbleEvent);
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message);
+                oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+            }
+        }
+
+       
+        void oApplication_FormDataEvent( ref SAPbouiCOM.BusinessObjectInfo BusinessObjectInfo, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+            try
+            {
+                //if (BusinessObjectInfo.FormTypeEx == SAPCustomFormUIDEnum.BOUR.ToString())
+                //{
+                //    objBOUR.FormDataEvent(ref BusinessObjectInfo, out BubbleEvent);
+                //}
+                //else if (BusinessObjectInfo.FormTypeEx == SAPCustomFormUIDEnum.DCBA.ToString())
+                //{
+                //    objDCBA.FormDataEvent(ref BusinessObjectInfo, out BubbleEvent);
+                //}
+                //else if(BusinessObjectInfo.FormTypeEx == Convert.ToString((int)SAPCustomFormUIDEnum.SalesOrder))
+                //{
+                //    objclsSales.FormDataEvent(ref BusinessObjectInfo, out BubbleEvent);
+                //}
+            }
+            catch (Exception ex)
+            {
+                SAPMain.logger.Error(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message);
+                oApplication.StatusBar.SetText(this.GetType().Name + " > " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + ex.Message, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+            }
+        }     
+        void oApplication_AppEvent(SAPbouiCOM.BoAppEventTypes EventType)
+        {
+            switch (EventType)
+            {
+
+
+                case SAPbouiCOM.BoAppEventTypes.aet_ShutDown:
+                case SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged:
+                case SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition:
+                case SAPbouiCOM.BoAppEventTypes.aet_LanguageChanged:
+                    oApplication.SetStatusBarMessage(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " Addon... Shutdown Event has been caught" + Environment.NewLine + "Terminating Add On...", BoMessageTime.bmt_Short, false);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(oCompany);
+
+                    System.Windows.Forms.Application.Exit();
+                    break;
+            }
+        }
+
+        void oApplication_LayoutKeyEvent(ref LayoutKeyInfo eventInfo, out bool BubbleEvent)
+        {
+            BubbleEvent = true;
+
+            if (eventInfo.BeforeAction == true)
+            {
+                //if (eventInfo.FormUID.Contains(SAPCustomFormUIDEnum.CCBI.ToString())
+                //   )
+                //{
+                //    eventInfo.LayoutKey = clsVariables.DocEntry;
+                //}
+
+
+            }
+        }
+     
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Configure log4net system based on application configuration setting
+        /// </summary>
+        private static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+            ((Hierarchy)LogManager.GetRepository()).RaiseConfigurationChanged(EventArgs.Empty);
+            logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            logger.Info("Logger initialzed.");
+        }
+
+        #endregion
+    }
+}
